dda7c885827051b2e5c4d78d6f88485bbb77c922
Murilo Nascimento <murilojesus659@gmail.com>
2023-06-13 17:07:22 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { useState } from 'react';
 import Todo from './components/Todo';
 import TodoForm from './components/TodoForm';
 import Search from './components/Search';
+import Filter from './components/Filter';
 
 import './App.css';
-import Filter from './components/Filter';
 
 function App() {
   const [todos, setTodos] = useState([
